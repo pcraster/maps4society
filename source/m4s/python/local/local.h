28f0af05142ff1ebf8369346c3a9e5cc10348f25
Oliver Schmitz <o.schmitz@uu.nl>
2015-07-01 10:55:49 +0200

    Fix compilation errors

--- source/m4s/python/local/local.h
+++ source/m4s/python/local/local.h
@@ -4,29 +4,29 @@
 // #include "m4s/python/local/or.h"
 // #include "m4s/python/local/not.h"
 // #include "m4s/python/local/xor.h"
-#include "m4s/python/local/defined.h"
+//#include "m4s/python/local/defined.h"
 
 
-#include "m4s/python/local/add.h"
-#include "m4s/python/local/sub.h"
+//#include "m4s/python/local/add.h"
+//#include "m4s/python/local/sub.h"
 #include "m4s/python/local/mul.h"
-#include "m4s/python/local/div.h"
+//#include "m4s/python/local/div.h"
 
 
 
-#include "m4s/python/local/pow.h"
-#include "m4s/python/local/cos.h"
+//#include "m4s/python/local/pow.h"
+//#include "m4s/python/local/cos.h"
 //#include "m4s/python/local/sin.h"
 //#include "m4s/python/local/tan.h"
-#include "m4s/python/local/sqrt.h"
+//#include "m4s/python/local/sqrt.h"
 
 
 //#include "m4s/python/local/.h"
 //#include "m4s/python/local/.h"
 
-#include "m4s/python/local/equal.h"
+//#include "m4s/python/local/equal.h"
 // #include "m4s/python/local/unequal.h"
-#include "m4s/python/local/less.h"
+//#include "m4s/python/local/less.h"
 // #include "m4s/python/local/less_equal.h"
 // #include "m4s/python/local/greater.h"
 // #include "m4s/python/local/greater_equal.h"
