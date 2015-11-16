bd3f78232b07c7befecd0ae716b1777257ab716f
Kor de Jong <k.dejong1@uu.nl>
2015-11-16 10:59:55 +0100

    Fix issues

--- environment/script/peacock.sh
+++ environment/script/peacock.sh
@@ -37,43 +37,39 @@ function parse_commandline()
 
 function copy_headers()
 {
-  # todo: other architectures?
-  target_dir=$prefix/linux/linux/gcc-4/x86_64/include/pcrtree2/
-  source_dir=$prefix/pcraster-head-Release-prefix/src/pcraster-head-Release/source
-  echo $target_dir
-  mkdir -p $target_dir
+    # TODO: other architectures?
+    target_dir=$prefix/linux/linux/gcc-4/x86_64/include/pcrtree2/
+    source_dir=./pcraster-head-Release-prefix/src/pcraster-head-Release/source
 
-  cp \
-$source_dir/pcraster_model_engine/calc_spatial.h \
-$source_dir/pcraster_model_engine/calc_nonspatial.h \
-$source_dir/pcraster_model_engine/calc_cr.h \
-$source_dir/pcraster_model_engine/calc_vs.h \
-$source_dir/pcraster_model_engine/calc_types.h \
-$source_dir/pcraster_model_engine/calc_field.h \
-$source_dir/pcraster_model_engine/calc_datavalue.h \
-$source_dir/pcraster_model_engine/calc_ovs.h \
-$source_dir/pcraster_python/Globals.h \
-$source_dir/pcrgeo/geo_rasterspace.h \
-$source_dir/pcrgeo/geo_def.h \
-$source_dir/pcrgeo/geo_rasterdim.h \
-$source_dir/pcrgeo/geo_cellloc.h \
-$source_dir/pcrcom/com_mvop.h \
-$source_dir/pcrcom/com_csfcell.h \
-$source_dir/app/appargs.h \
-$source_dir/include/pcrdatatype.h \
-$source_dir/include/environ.h \
-$source_dir/include/stddefx.h \
-$source_dir/include/pcrdll.h \
-$source_dir/include/typedef.h \
-$source_dir/include/debug.h \
-$source_dir/include/dev_Compiler.h \
-$source_dir/pcraster_dal/dal_Client.h \
-$source_dir/pcraster_dal/dal_Configure.h \
-$target_dir
+    mkdir -p $target_dir
 
-  
-
-
+    cp \
+        $source_dir/pcraster_model_engine/calc_spatial.h \
+        $source_dir/pcraster_model_engine/calc_nonspatial.h \
+        $source_dir/pcraster_model_engine/calc_cr.h \
+        $source_dir/pcraster_model_engine/calc_vs.h \
+        $source_dir/pcraster_model_engine/calc_types.h \
+        $source_dir/pcraster_model_engine/calc_field.h \
+        $source_dir/pcraster_model_engine/calc_datavalue.h \
+        $source_dir/pcraster_model_engine/calc_ovs.h \
+        $source_dir/pcraster_python/Globals.h \
+        $source_dir/pcrgeo/geo_rasterspace.h \
+        $source_dir/pcrgeo/geo_def.h \
+        $source_dir/pcrgeo/geo_rasterdim.h \
+        $source_dir/pcrgeo/geo_cellloc.h \
+        $source_dir/pcrcom/com_mvop.h \
+        $source_dir/pcrcom/com_csfcell.h \
+        $source_dir/app/appargs.h \
+        $source_dir/include/pcrdatatype.h \
+        $source_dir/include/environ.h \
+        $source_dir/include/stddefx.h \
+        $source_dir/include/pcrdll.h \
+        $source_dir/include/typedef.h \
+        $source_dir/include/debug.h \
+        $source_dir/include/dev_Compiler.h \
+        $source_dir/pcraster_dal/dal_Client.h \
+        $source_dir/pcraster_dal/dal_Configure.h \
+        $target_dir
 }
 
 
@@ -105,7 +101,8 @@ function build_software()
     # Fern.
     options+=("-Dbuild_fern=true")
     options+=("-Dfern_git_repository=https://github.com/geoneric/fern.git")
-    options+=("-Dfern_git_tag=d90ececdcaf365e608009aaa3c4c7a30027e0249")
+    # options+=("-Dfern_git_tag=d90ececdcaf365e608009aaa3c4c7a30027e0249")
+    options+=("-Dfern_git_tag=47cd1676bcbef67b04fe3cb0fd310366764571ad")
     options+=("-Dfern_build_fern_algorithm=true")
     options+=("-Dfern_build_fern_documentation=true")
     options+=("-Dfern_build_fern_test=true")
@@ -133,15 +130,16 @@ function build_software()
     options+=(
         "-Dpcraster_git_repository=https://github.com/pcraster/pcraster.git")
     options+=("-Dpcraster_git_tag=f1b54c6b5d6b0006543b05978bb0b4abc5bf5ad1")
+    # CMake Error at source/modflow/documentation/cmake_install.cmake:36 (file):
+    #   file INSTALL cannot find
+    #     "/home/shared1/builder/tmp/peacock/pcraster-head-Release-prefix/src/pcraster-head-Release-build/source/modflow/documentation/_build/html".
+    # options+=("-Dpcraster_git_tag=9d73caee948454aac8f923d1013c7ee4e0afd539")
     options+=("-Dpcraster_build_pcraster_documentation=true")
     options+=("-Dpcraster_build_pcraster_test=true")
 
 
-#    cmake "${options[@]}" $source
-#    cmake --build . --target all
-    echo    $download_dir
-    echo $prefix
-    echo $source
+    cmake "${options[@]}" $source
+    cmake --build . --target all
 
     copy_headers
 }
